c85909f4df09812eb50b1b8e9e76e8f7370df571
DSUSawyerDeWitt <156718843+DSUSawyerDeWitt@users.noreply.github.com>
2025-02-12 14:52:32 -0600

    Added Pong Files

---
+++ Pong/code/settings.py
@@ -0,0 +1,13 @@
+import pygame
+from os.path import join
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 900, 600
+SIZE = {'paddle': (40,100), 'ball': (30,30)}
+POS = {'player': (WINDOW_WIDTH - 50, WINDOW_HEIGHT / 2), 'opponent': (50, WINDOW_HEIGHT / 2)}
+SPEED = {'player': 500, 'opponent': 250, 'ball': 300}
+COLORS = {
+    'paddle': '#ee322c',
+    'ball': '#ee622c',
+    'bg': '#002633',
+    'bg detail': '#004a63'
+}
